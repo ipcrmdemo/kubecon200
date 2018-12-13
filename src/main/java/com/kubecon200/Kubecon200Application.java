2ad20b3708f7aca0d4dc9edd8e38d3058305f931
Matt Cadorette <mcadorette@gmail.com>
2018-12-13 10:15:26 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/kubecon200/Kubecon200Application.java
@@ -0,0 +1,12 @@
+package com.kubecon200;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kubecon200Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kubecon200Application.class, args);
+	}
+}
